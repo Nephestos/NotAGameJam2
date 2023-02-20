88fa6dd625d025c327d6160d992b9ed8dc3fb98c
eremus <vincent.amiot@etudiant.univ-rennes1.fr>
2023-02-20 15:32:54 +0100

    Mise en place changement de scene + Player+ cave + début village

---
+++ player/2dPlayer.gd
@@ -0,0 +1,30 @@
+extends KinematicBody2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+export (int) var speed = 200
+
+var velocity = Vector2()
+
+func get_input():
+	velocity = Vector2()
+	if Input.is_action_pressed("right"):
+		velocity.x += 1
+	if Input.is_action_pressed("left"):
+		velocity.x -= 1
+	velocity = velocity.normalized() * speed
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	get_input()
+	velocity = move_and_slide(velocity)
+
